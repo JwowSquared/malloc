6fd9c1921fc62630d650ced447bfb1f25017d6ab
JwowSquared <Jdwagner1@hotmail.com>
2022-01-05 08:58:04 +0000

    added task 0 source code

---
+++ malloc.h
@@ -0,0 +1,10 @@
+#ifndef _MALLOC_H_
+#define _MALLOC_H_
+
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+
+void *naive_malloc(size_t size);
+
+#endif /* _MALLOC_H_ */
---
+++ naive_malloc.c
@@ -0,0 +1,46 @@
+#include "malloc.h"
+
+/**
+* naive_malloc - malloc that only allocates size memory
+* @size: bytes to allocate
+*
+* Return: address of new memory location
+*/
+void *naive_malloc(size_t size)
+{
+	static void *start;
+	static void *end;
+	static size_t avail_bytes;
+	size_t h_size = sizeof(size_t);
+	void *out;
+
+	if (start == NULL)
+		start = sbrk(0);
+
+	while (avail_bytes < size + h_size)
+	{
+		if (sbrk(1024) == (void *)-1)
+			return (NULL); /* not enough memory */
+		avail_bytes += 1024;
+	}
+
+	out = start;
+	if (end != NULL)
+	{
+		while (out != end)
+		{
+			/* check previously malloc'd sections for viability */
+			out = (char *)out + h_size + *(size_t *)out;
+		}
+		if (out == end)
+		{
+			out = (char *)out + h_size + *(size_t *)out;
+			end = out;
+		}
+	}
+	else
+		end = out;
+
+	*(size_t *)out = size;
+	return ((char *)out + h_size);
+}
